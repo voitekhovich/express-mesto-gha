66fa7aa49c7a4e43d774a5133366997c45a46e03
voitekhovich <a.voitekhovich@gmail.com>
2022-11-06 23:30:57 +0300

    feat: Добавить логгер

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-
+const helmet = require('helmet');
 const errs = require('./middlewares/errs');
+const { limiter } = require('./utils/rateLimiter');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_DB_URL = 'mongodb://localhost:27017/mestodb2' } = process.env;
 const app = express();
 
 mongoose.connect(MONGO_DB_URL);
 
+app.use(limiter);
+app.use(helmet());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(require('./routes'));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errs);
